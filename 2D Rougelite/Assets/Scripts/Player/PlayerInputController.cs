e906aa8845eb275a2faa942a3560f30bc32944be
Kody <jdgomez512@gmail.com>
2026-03-27 21:53:46 -0600

    Retry

--- 2D Rougelite/Assets/Scripts/Player/PlayerInputController.cs
+++ 2D Rougelite/Assets/Scripts/Player/PlayerInputController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Player_Controlls : MonoBehaviour
+public class PlayerInputController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
